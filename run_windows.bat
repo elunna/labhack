849b5f3c5eb35fc6ad92b7d169f2be633116c95b
Erik Lunna <eslunna@gmail.com>
2021-01-29 15:36:44 -0600

    Fixed run_windows.bat

--- run_windows.bat
+++ run_windows.bat
@@ -1,5 +1,6 @@
-# Simple, tiny script to run the program from the root directory in Windows
+REM Simple, tiny script to run the program from the root directory in Windows
 
 @ECHO OFF
 cd src
 python main.py
+cd ..
