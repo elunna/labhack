7a7ad6b84e8221fdcfa9d1b0a19afc19c7a1188a
Erik Lunna <eslunna@gmail.com>
2021-01-31 15:29:17 -0600

    Added test_map fixture to test_gamemap.py

--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -3,66 +3,62 @@
 import pytest
 import game_map
 
-def test_GameMap_init():
-    g = game_map.GameMap(width=10, height=15)
-    assert g.width == 10
-    assert g.height == 15
-    assert g.entities == set()
+@pytest.fixture
+def test_map():
+    return game_map.GameMap(width=10, height=15)
+
+def test_GameMap_init(test_map):
+    assert test_map.width == 10
+    assert test_map.height == 15
+    assert test_map.entities == set()
 
     # g.tiles
     # TODO: Test that all the starting tiles are walls.
-    assert len(g.tiles) == 10
+    assert len(test_map.tiles) == 10
 
     # g.visible
-    assert len(g.visible) == 10
-    assert not g.visible.all()  # By default none should be visible
+    assert len(test_map.visible) == 10
+    assert not test_map.visible.all()  # By default none should be visible
 
     # self.explored
-    assert len(g.explored) == 10
-    assert not g.explored.all()  # By default none should be explored
+    assert len(test_map.explored) == 10
+    assert not test_map.explored.all()  # By default none should be explored
 
     # This should default to 0,0
-    assert g.downstairs_location == (0, 0)
+    assert test_map.downstairs_location == (0, 0)
 
 
-def test_GameMap_gamemap():
-    g = game_map.GameMap(engine=None, width=10, height=15)
-    assert g.gamemap is g
+def test_GameMap_gamemap(test_map):
+    assert test_map.gamemap is test_map
 
 
-def test_GameMap_actors__none_by_default():
-    g = game_map.GameMap(engine=None, width=10, height=15)
+def test_GameMap_actors__none_by_default(test_map):
     # We get a generator, need to convert to list.
-    assert list(g.actors) == []
+    assert list(test_map.actors) == []
 
 
-def test_GameMap_items__none_by_default():
-    g = game_map.GameMap(engine=None, width=10, height=15)
+def test_GameMap_items__none_by_default(test_map):
     # We get a generator, need to convert to list.
-    assert list(g.items) == []
+    assert list(test_map.items) == []
 
 
 # TODO: Test with walkable tile
 # TODO: Test with blocking entity
-def test_GameMap_get_blocking_entity_at_location():
-    g = game_map.GameMap(engine=None, width=10, height=15)
-    assert g.get_blocking_entity_at_location(0, 0) is None
+def test_GameMap_get_blocking_entity_at_location(test_map):
+    assert test_map.get_blocking_entity_at_location(0, 0) is None
 
 
 # TODO: Test with walkable tile
 # TODO: Test with blocking entity
-def test_GameMap_get_actor_at_location():
-    g = game_map.GameMap(engine=None, width=10, height=15)
-    assert g.get_actor_at_location(0, 0) is None
+def test_GameMap_get_actor_at_location(test_map):
+    assert test_map.get_actor_at_location(0, 0) is None
 
 
-def test_GameMap_in_bounds__valid_loc():
-    g = game_map.GameMap(engine=None, width=10, height=15)
-    assert g.in_bounds(0, 0)
-    assert g.in_bounds(9, 14)
+def test_GameMap_in_bounds__valid_loc(test_map):
+    assert test_map.in_bounds(0, 0)
+    assert test_map.in_bounds(9, 14)
 
 
-def test_GameMap_in_bounds__invalid_loc():
-    g = game_map.GameMap(engine=None, width=10, height=15)
-    assert not g.in_bounds(-1, -1)
-    assert not g.in_bounds(10, 15)
+def test_GameMap_in_bounds__invalid_loc(test_map):
+    assert not test_map.in_bounds(-1, -1)
+    assert not test_map.in_bounds(10, 15)
