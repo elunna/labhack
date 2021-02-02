ef511bb59b7fb27c17b4c657d194e7d186b1645c
Erik Lunna <eslunna@gmail.com>
2021-02-02 15:27:37 -0600

    Imported my Entity from a previous project, tests work.

---
+++ new_entity.py
@@ -0,0 +1,76 @@
+class Entity(object):
+    """ A generic object to represent players, enemies, items, etc.
+        We use a dictionary to manage the entity's Components.
+        A component must be of type: BaseComponent.
+        All entities have some base info:
+            x, y, char, name, color
+            x, y can be a PositionComponent
+            name can be a NameComponent (along with unidentified appearence)
+            char, color can be DisplayComponent
+
+        This structure with getattr and setattr allows us to access attributes
+        of components easily from the entity, without subcalling the components
+        all the time.
+    """
+
+    def __init__(self, **kwargs):
+        self.components = kwargs
+
+    def __str__(self):
+        if self.has_comp('name'):
+            return self.name
+        return 'Unnamed'
+
+    def __getattr__(self, name):
+        if name in self.components:
+            return self.components[name]
+
+        raise AttributeError('Entity has no component with attribute {}'.format(name))
+
+    def __setattr__(self, key, value):
+        if key == 'components':
+            # self.components = value
+            super().__setattr__('components', value)
+        else:
+            self.components[key] = value
+
+    def __getstate__(self):
+        """But if we try to pickle our d instance, we get RecursionError because
+            of that __getattr__ which does the magic conversion of attribute
+            access to key lookup. We can overcome that by providing the class
+            with __getstate__ and __setstate__ methods.
+            https://stackoverflow.com/questions/50156118/recursionerror-maximum-recursion-depth-exceeded-while-calling-a-python-object-w/50158865#50158865
+        """
+        return self.components
+
+    def __setstate__(self, state):
+        """See comment for __getstate__"""
+        self.components = state
+
+    def add_comp(self, **kwargs):
+        for k, v in kwargs.items():
+            self.components[k] = v
+
+    def has_comp(self, component):
+        if component in self.components:
+            return True
+        return False
+
+    def rm_comp(self, component):
+        if component in self.components:
+            self.components.pop(component)
+            return True
+        return False
+
+    def move(self, dx, dy):
+        # Move the entity by a given amount
+        dest_x = self.x + dx
+        dest_y = self.y + dy
+
+        if dest_x < 0 or dest_y < 0:
+            raise ValueError('move cannot place entity in a negative x or y: ({}, {})'.format(dest_x, dest_y))
+        self.x += dx
+        self.y += dy
+
+    def distance_to(self, other):
+        return stages.Stage.distance_between_entities(self, other)
---
+++ tests/test_new_entity.py
@@ -0,0 +1,122 @@
+import pytest
+from new_entity import Entity
+
+
+def test_Entity_init__defaults():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    assert e.x == 0
+    assert e.y == 0
+    assert e.char == '@'
+    assert e.color is None
+    assert e.name == 'Player'
+
+
+def test_Entity_init__components_dict():
+    e = Entity(x=0, y=0)
+    assert e.components == {'x': 0, 'y': 0}
+
+
+@pytest.mark.skip(reason='implement after ecs is mostly done.')
+def test_Entity_init__kwargs_become_components():
+    pass
+
+
+def test_Entity_str__has_name():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    assert str(e) == 'Player'
+
+
+def test_Entity_str__unnamed():
+    e = Entity(x=0, y=0, char='@', color=None)
+    assert str(e) == 'Unnamed'
+
+
+def test_Entity_init__add_comp__1_kwarg():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    assert e.components['a'] == 1
+
+
+def test_Entity_init__add_comp__2_kwargs():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1, b=2)
+    assert e.components['a'] == 1
+    assert e.components['b'] == 2
+
+
+def test_Entity_init__add_comp__already_exists_and_replaces():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    e.add_comp(a=2)
+    assert e.components['a'] == 2
+
+
+def test_Entity_init__has_comp():
+    e = Entity(x=0, y=0)
+    assert e.has_comp('x')
+    assert e.has_comp('y')
+
+
+def test_Entity_init__rm_comp__success_removes_component():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    e.rm_comp('a')
+    assert 'a' not in e.components
+
+
+def test_Entity_init__rm_comp__success_returns_True():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    result = e.rm_comp('a')
+    assert result
+
+
+def test_Entity_init__rm_comp__fail_returns_False():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    result = e.rm_comp('z')
+    # Raise exception?
+
+
+def test_Entity_init__getattr__returns_component_value():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    assert e.a == 1
+
+
+def test_Entity_init__getattr__DNE_returns_None():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    with pytest.raises(AttributeError):
+        result = e.a
+
+
+def test_Entity_init__blocks_component():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player', blocks=True)
+
+    assert e.blocks
+    assert 'blocks' in e.components
+
+
+def test_Entity_move__to_a_negative_x():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    with pytest.raises(ValueError):
+        e.move(-1, 2)
+
+
+def test_Entity_move__to_a_negative_y():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    with pytest.raises(ValueError):
+        e.move(1, -2)
+
+
+def test_Entity_move__positive_values():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.move(1, 2)
+    assert e.x == 1
+    assert e.y == 2
+
+
+def test_Entity_move__negative_values():
+    e = Entity(x=10, y=10, char='@', color=None, name='Player')
+    e.move(-1, 2)
+    assert e.x == 9
+    assert e.y == 12
