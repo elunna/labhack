f200021038a2e07e8043f3a055ff4420b9980eb9
Erik Lunna <eslunna@gmail.com>
2021-02-01 19:08:41 -0600

    Added tests for get_max_value_for_floor

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -11,10 +11,28 @@ item_chances = {
     2: [('a', 15), ('d', 5)],
 }
 
-@pytest.mark.skip(reason='Create sample tables for testing')
-def test_get_max_value_for_floor():
-    pass
+max_foos_by_floor = [
+    (0, 1), (2, 2), (3, 3), (5, 5)
+]
 
+
+def test_get_max_value_for_floor__negative():
+    result = factories.get_max_value_for_floor(max_foos_by_floor, -1)
+    assert result == 0
+
+def test_get_max_value_for_floor__listed_floor():
+    result = factories.get_max_value_for_floor(max_foos_by_floor, 0)
+    assert result == 1
+
+
+def test_get_max_value_for_floor__inbetween_floor():
+    result = factories.get_max_value_for_floor(max_foos_by_floor, 1)
+    assert result == 1
+
+
+def test_get_max_value_for_floor__higher_floor():
+    result = factories.get_max_value_for_floor(max_foos_by_floor, 1000)
+    assert result == 5
 
 @pytest.mark.skip(reason='Create sample tables for testing')
 def test_get_entities_at_random():
