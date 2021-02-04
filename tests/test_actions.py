646a20895315d69b36da90f0ad3bc01663a9877a
Erik Lunna <eslunna@gmail.com>
2021-02-04 16:36:50 -0600

    Added more tests for ActionWithDirection.

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -29,11 +29,40 @@ def test_Action_perform__not_implemented(player):
     with pytest.raises(NotImplementedError):
         a.perform()
 
-# ActionWithDirection
-# init, is Action
-# dest_xy
-# blocking_entity
-# target_actor
+
+def test_ActionWithDirection_init(player):
+    a = actions.ActionWithDirection(entity=player, dx=1, dy=-1)
+    assert isinstance(a, actions.Action)
+    assert a.entity == player
+    assert a.dx == 1
+    assert a.dy == -1
+    assert a.msg == ''
+
+
+def test_ActionWithDirection_dest_xy(player):
+    dx, dy = 1, -1
+    a = actions.ActionWithDirection(entity=player, dx=dx, dy=dy)
+    assert player.x == 0
+    assert player.y == 0
+    assert a.dest_xy == (1, -1)
+
+    # more dynamic version
+    assert a.dest_xy == (player.x + dx, player.y + dy)
+
+def test_ActionWithDirection_blocking_entity(player):
+    testmap = factories.test_map()
+    player.place(1, 2, testmap)
+    # Blocked by a wall, not an entity
+    a = actions.ActionWithDirection(entity=player, dx=0, dy=-1)
+    assert a.blocking_entity is None
+
+def test_ActionWithDirection__target_actor(player):
+    testmap = factories.test_map()
+    player.place(2, 4, testmap)
+    a = actions.ActionWithDirection(entity=player, dx=0, dy=1)
+    assert a.target_actor.name == "grid bug"
+
+
 # perform
 
 # BumpAction
