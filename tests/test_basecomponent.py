d3c9e45bcf7d7de164dd4a214af211074c3650c8
Erik Lunna <eslunna@gmail.com>
2021-02-05 00:27:27 -0600

    BaseComponent: Added tests

---
+++ tests/test_basecomponent.py
@@ -0,0 +1,21 @@
+""" Tests for component.py """
+
+from components.base_component import BaseComponent
+import pytest
+
+
+def test_BaseComponent__init():
+    c = BaseComponent()
+    assert c.parent is None
+
+
+@pytest.mark.skip(reason='Annoying')
+def test_BaseComponent__gamemap__no_parent():
+    c = BaseComponent()
+    assert c.gamemap is None
+
+
+@pytest.mark.skip(reason='Annoying')
+def test_BaseComponent__engine__no_parent():
+    c = BaseComponent()
+    assert c.engine is None
