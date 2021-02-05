9687af6048260bd98d7011ce00005ab2d3c7112d
Erik Lunna <eslunna@gmail.com>
2021-02-05 00:35:52 -0600

    Inventory: Added a few tests.

---
+++ tests/test_inventory.py
@@ -0,0 +1,27 @@
+""" Tests for inventiry.py """
+
+from components.base_component import BaseComponent
+from components.inventory import Inventory
+import pytest
+
+
+def test_Inventory_is_BaseComponent():
+    i = Inventory(10)
+    assert isinstance(i, BaseComponent)
+
+
+def test_Inventory_init():
+    i = Inventory(10)
+    assert i.capacity == 10
+    assert i.items == []
+
+
+@pytest.mark.skip(reason='Needs item, etc.')
+def test_Inventory_drop__invalid_item():
+    i = Inventory(10)
+    pass
+
+
+@pytest.mark.skip(reason='Needs gamemap setup')
+def test_Inventory_drop__valid_item():
+    i = Inventory(10)
