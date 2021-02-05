2c5f212ceb87e3f496070dee3d38f3dd3672b649
Erik Lunna <eslunna@gmail.com>
2021-02-05 12:52:58 -0600

    consumable.py: Skeleton tests.

---
+++ tests/test_consumable.py
@@ -0,0 +1,94 @@
+""" Tests for consumable.py """
+
+@pytest.mark.skip(reason='Skeleton')
+def test_Consumable__is_Component():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_Consumable_init():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_Consumable_get_action():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_Consumable_activate():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_Consumable_consume():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_HealingConsumable__is_Component():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_HealingConsumable__is_Consumable():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_HealingConsumable_init():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_HealingConsumable_activate():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_LightningDamageConsumable__is_Component():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_LightningDamageConsumable__is_Consumable():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_LightningDamageConsumable_init():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_LightningDamageConsumable_activate():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_ConfusionConsumable__is_Component():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_ConfusionConsumable__is_Consumable():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_ConfusionConsumable_init():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_ConfusionConsumable_get_action():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_ConfusionConsumable_activate():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_FireballDamageConsumable__is_Component():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_FireballDamageConsumable__is_Consumable():
+    pass
+
+def test_FireballDamageConsumable_init():
+@pytest.mark.skip(reason='Skeleton')
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_FireballDamageConsumable_get_action():
+    pass
+
+@pytest.mark.skip(reason='Skeleton')
+def test_FireballDamageConsumable_activate():
+    pass
+
