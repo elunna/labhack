655455faedc8ff020a9e03ec337d2a4bbbcf2040
Erik Lunna <eslunna@gmail.com>
2021-02-04 23:18:10 -0600

    exceptions.py: Added tests.

---
+++ tests/test_exceptions.py
@@ -0,0 +1,12 @@
+""" Tests for actors.py """
+from src import exceptions
+
+
+def test_Impossible__is_Exception():
+    i = exceptions.Impossible()
+    assert isinstance(i, Exception)
+
+
+def test_QuitWithoutSaving__is_SystemExit():
+    i = exceptions.QuitWithoutSaving()
+    assert isinstance(i, SystemExit)
