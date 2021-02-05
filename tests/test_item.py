3274273de1bb91528031d4d5462ec28136a591a3
Erik Lunna <eslunna@gmail.com>
2021-02-04 23:04:18 -0600

    Item: Added tests

---
+++ tests/test_item.py
@@ -0,0 +1,22 @@
+""" Tests for items.py """
+from src.settings import RenderOrder
+from src import entity
+from src import item
+
+
+def test_Item_subclass_of_Entity():
+    i = item.Item()
+    assert isinstance(i, entity.Entity)
+
+
+def test_Item_init_defaults():
+    i = item.Item()
+    assert i.x == 0
+    assert i.y == 0
+    assert i.char == '?'
+    assert i.color == (255, 255, 255)
+    assert i.name == "<Unnamed>"
+    assert i.consumable is None
+    assert i.equippable is None
+    assert i.blocks_movement is False
+    assert i.render_order == RenderOrder.ITEM
