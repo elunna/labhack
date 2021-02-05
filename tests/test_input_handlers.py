6923e4b4bc86f4bd5469e38dd04058dcb2d7ce31
Erik Lunna <eslunna@gmail.com>
2021-02-05 12:10:34 -0600

    input_handlers: Added skeleton for tests

---
+++ tests/test_input_handlers.py
@@ -0,0 +1,82 @@
+""" Tests for input_handlers.py """
+
+# ActionOrHandler
+
+# BaseEventHandler
+#   init
+#   handle_events
+#   on_render
+#   ev_quit
+
+# EventHandler
+#   init
+#   handle_events
+#   handle_action
+#   ev_mousemotion
+#   on_render
+
+# MainGameEventHandler
+#   ev_keydown
+
+# GameOverEventHandler
+#   on_quit
+#   ev_quit
+#   ev_keydown
+
+# HistoryViewer
+#   init
+#   on_render
+#   ev_keydown
+
+# AskUserEventHandler
+#   ev_keydown
+#   ev_mousebuttondown
+#   on_exit
+
+# InventoryEventHandler
+#   on_render
+#   ev_keydown
+#   on_item_selected
+
+# InventoryActivateHandler
+#   on_item_selected
+
+# InventoryDropHandler
+#   TITLE
+#   on_item_selected
+
+# SelectIndexHandler
+#   on_render
+#   ev_keydown
+#   ev_mousebuttondown
+#   on_index_selected
+
+
+
+# LookHandler
+#   on_index_selected
+
+# SingleRangedAttackHandler
+#   on_index_selected
+
+# AreaRangedAttackHandler
+#   on_render
+#   on_index_selected
+
+# PopupMessage
+#   on_render
+#   ev_keydown
+
+# LevelUpEventHandler
+#   on_render
+#   ev_keydown
+#   ev_mousebuttondown
+
+# CharacterScreenEventHandler
+#   on_render
+
+# MainMenuHandler
+#   on_render
+#   ev_keydown
+
+
