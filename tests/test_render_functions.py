3a4d4006fb9cb0ba1c00b933c4dddb80cff882d7
Erik Lunna <eslunna@gmail.com>
2021-02-05 12:15:32 -0600

    render_functions: Added skeleton for tests.

---
+++ tests/test_render_functions.py
@@ -0,0 +1,15 @@
+""" Tests for render_functions.py"""
+# render_bar
+
+# render_dungeon_lvl_text
+
+# get_names_at_location
+
+# render_names_at_mouse_location
+
+# render_messages
+
+# render_map
+
+
+
