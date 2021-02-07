fa8e0949e0d83ffd4ac05e0a491d84aefb37b345
Erik Lunna <eslunna@gmail.com>
2021-02-07 10:06:21 -0600

    Updated attacks.py description.

--- components/attacks.py
+++ components/attacks.py
@@ -2,10 +2,19 @@ import random
 
 
 class AttackType:
-    def __init__(self, die_sides, description='hits'):
-        self.die_sides = die_sides
-        self.description = description
+    """ Describes an Actors attack types and corresponding damage dies.
+        bite: d6
+        hit: d2
+        punch: d2
 
-    def roll_attack(self):
+        We might be able to create lambdas for more advanced calculations.
+        claw: 2d6 + 1
+    """
+    def __init__(self, description, die_sides):
+        self.description = description
+        self.die_sides = die_sides
+
+    def roll_attacks(self):
+        # Generator to roll through all attacks
         # Simulates a die roll for the attack die.
         return random.randint(1, self.die_sides)
