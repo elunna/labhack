fa0405d08d31d496c7159b1d5ca1d066242c4efb
Erik Lunna <eslunna@gmail.com>
2021-02-07 21:12:03 -0600

    Fixed a broken test in test_inventory.py

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -13,8 +13,21 @@ def test_Inventory_is_BaseComponent():
 def test_Inventory_init():
     i = Inventory(10)
     assert i.capacity == 10
-    assert i.items == []
+    assert i.items == {}
 
+
+def test_Inventory_init__first_inv_letter_is_a():
+    i = Inventory(10)
+    assert i.current_letter == 'a'
+
+# add item
+# first letter is a
+# seconds letter is b
+# cannot add more than capacity
+
+# rm_letter
+# letter in items
+# letter not in items
 
 @pytest.mark.skip(reason='Needs item, etc.')
 def test_Inventory_drop__invalid_item():
@@ -25,3 +38,14 @@ def test_Inventory_drop__invalid_item():
 @pytest.mark.skip(reason='Needs gamemap setup')
 def test_Inventory_drop__valid_item():
     i = Inventory(10)
+
+# sorted_dict
+# Make sure it sorts a diverse inventory by group
+
+
+# def test_LetterRoll__init():
+# self.letters = 'abcdefghijklmnopqrstuvwxyz'
+# index = -1
+# first letter is a
+# next letter is b
+# build a roll from a-z then a
