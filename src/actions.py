bdf6f3ab61f1bb542197faf5f6c96acc1e495a9e
Erik Lunna <eslunna@gmail.com>
2021-02-08 11:04:04 -0600

    PickupAction: Added tests.

--- src/actions.py
+++ src/actions.py
@@ -211,19 +211,22 @@ class PickupAction(Action):
         super().__init__(entity)
 
     def perform(self):
+        # TODO: Support for piles
+        # TODO: Pickup menu handler
+
         actor_location_x = self.entity.x
         actor_location_y = self.entity.y
         inventory = self.entity.inventory
 
-        for item in self.engine.game_map.items:
+        for item in self.entity.gamemap.items:
             if actor_location_x == item.x and actor_location_y == item.y:
                 if len(inventory.items) >= inventory.capacity:
                     raise exceptions.Impossible("Your inventory is full.")
 
-                self.engine.game_map.entities.remove(item)
+                self.entity.gamemap.entities.remove(item)
                 item.parent = self.entity.inventory
                 inventory.add_item(item)
-                self.msg = f"You picked up the {item.name}!"
+                self.msg = f"You picked up the {item.name}. "
                 return
 
         raise exceptions.Impossible("There is nothing here to pick up.")
