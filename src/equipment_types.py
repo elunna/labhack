a772ab9e7b31ee55bc99c056e4c96bc617a4b411
Erik Lunna <eslunna@gmail.com>
2021-02-11 08:43:32 -0600

    Added some ideas to EquipmentTypes.

--- src/equipment_types.py
+++ src/equipment_types.py
@@ -4,3 +4,13 @@ from enum import Enum, auto
 class EquipmentType(Enum):
     WEAPON = auto()
     ARMOR = auto()
+    # HELMET
+    # SHIELD
+    # BOOTS
+    # GLOVES
+    # WRIST(R/L)
+    # BELT
+    # SUIT
+
+    # TODO: Slot over another slot
+    # Ex: Tshirt under armor, under cloak
