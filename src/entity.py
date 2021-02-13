401244b66f71e9fcdc05406f5d91302a00c877ea
Erik Lunna <eslunna@gmail.com>
2021-02-13 09:45:51 -0600

    Implemented the modular Entity with tests.

--- src/entity.py
+++ src/entity.py
@@ -1,43 +1,63 @@
-from .renderorder import RenderOrder
 import copy
 import math
 
 
-class Entity:
-    """ A generic object to represent players, enemies, items, etc."""
-    # parent: GameMap
-    # parent = None
-
-    def __init__(
-            self,
-            parent=None,  # Should be GameMap
-            x=0,
-            y=0,
-            char='?',
-            color=(255, 255, 255),
-            name='<Unnamed>',
-            blocks_movement=False,
-            render_order=RenderOrder.CORPSE,
-    ):
-        self.x = x
-        self.y = y
-        self.char = char
-        self.color = color  # color is a tuple
-        self.name = name
-        self.blocks_movement = blocks_movement
-        self.render_order = render_order
-
-        if parent:
-            # If parent isn't provided now then than will be set later.
-            self.parent = parent
-            parent.entities.add(self)
+class Entity(object):
+    """ A generic object to represent players, enemies, items, etc.
+        We use a dictionary to manage the entity's Components.
+    """
+    def __init__(self, **kwargs):
+        self.components = kwargs
 
     def __str__(self):
-        return self.name
+        if self.has_comp('name'):
+            return self.name
+        return 'Unnamed'
+
+    def __getattr__(self, name):
+        if name in self.components:
+            return self.components[name]
+
+        raise AttributeError('Entity has no component with attribute {}'.format(name))
+
+    def __setattr__(self, key, value):
+        if key == 'components':
+            # self.components = value
+            super().__setattr__('components', value)
+        else:
+            self.components[key] = value
+
+    def __getstate__(self):
+        """But if we try to pickle our d instance, we get RecursionError because
+            of that __getattr__ which does the magic conversion of attribute
+            access to key lookup. We can overcome that by providing the class
+            with __getstate__ and __setstate__ methods.
+            https://stackoverflow.com/questions/50156118/recursionerror-maximum-recursion-depth-exceeded-while-calling-a-python-object-w/50158865#50158865
+        """
+        return self.components
+
+    def __setstate__(self, state):
+        """See comment for __getstate__"""
+        self.components = state
 
     @property
     def gamemap(self):
         return self.parent.gamemap
+
+    def add_comp(self, **kwargs):
+        for k, v in kwargs.items():
+            self.components[k] = v
+
+    def has_comp(self, component):
+        if component in self.components:
+            return True
+        return False
+
+    def rm_comp(self, component):
+        if component in self.components:
+            self.components.pop(component)
+            return True
+        return False
 
     def move(self, dx, dy):
         # Move the entity by a given amount
--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,9 +1,6 @@
-""" Tests for entity.py """
-from components.fighter import Fighter
-from src.entity import Entity
-from src.renderorder import RenderOrder
-import toolkit
 import pytest
+from src.entity import Entity
+import toolkit
 
 
 @pytest.fixture
@@ -12,57 +9,97 @@ def test_map():
 
 
 def test_Entity_init__defaults():
-    e = Entity()
-    # assert e.parent is None
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
     assert e.x == 0
     assert e.y == 0
-    assert e.char == '?'
-    assert e.color == (255, 255, 255)
-    assert e.name == '<Unnamed>'
-    assert e.blocks_movement is False
-    assert e.render_order == RenderOrder.CORPSE
-
-
-def test_Entity_init__xy():
-    e = Entity(x=1, y=2)
-    assert e.x == 1
-    assert e.y == 2
-
-
-def test_Entity_init__char_and_color():
-    e = Entity(char='@', color='white')
     assert e.char == '@'
-    assert e.color == 'white'
-
-
-def test_Entity_init__name():
-    e = Entity(name='Player')
+    assert e.color is None
     assert e.name == 'Player'
 
 
-def test_Entity_init__gamemap(test_map):
-    e = Entity(parent=test_map)
-    assert e.parent == test_map
+def test_Entity_init__components_dict():
+    e = Entity(x=0, y=0)
+    assert e.components == {'x': 0, 'y': 0}
 
 
-def test_Entity_init__blocks_movement():
-    e = Entity()
-    assert e.blocks_movement is False
-
-
-def test_Entity_init__renderorder():
-    e = Entity()
-    assert e.render_order == RenderOrder.CORPSE
+@pytest.mark.skip(reason='implement after ecs is mostly done.')
+def test_Entity_init__kwargs_become_components():
+    pass
 
 
 def test_Entity_str__has_name():
-    e = Entity(name='Player')
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
     assert str(e) == 'Player'
 
 
 def test_Entity_str__unnamed():
-    e = Entity()
-    assert str(e) == '<Unnamed>'
+    e = Entity(x=0, y=0, char='@', color=None)
+    assert str(e) == 'Unnamed'
+
+
+def test_Entity_init__add_comp__1_kwarg():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    assert e.components['a'] == 1
+
+
+def test_Entity_init__add_comp__2_kwargs():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1, b=2)
+    assert e.components['a'] == 1
+    assert e.components['b'] == 2
+
+
+def test_Entity_init__add_comp__already_exists_and_replaces():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    e.add_comp(a=2)
+    assert e.components['a'] == 2
+
+
+def test_Entity_init__has_comp():
+    e = Entity(x=0, y=0)
+    assert e.has_comp('x')
+    assert e.has_comp('y')
+
+
+def test_Entity_init__rm_comp__success_removes_component():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    e.rm_comp('a')
+    assert 'a' not in e.components
+
+
+def test_Entity_init__rm_comp__success_returns_True():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    result = e.rm_comp('a')
+    assert result
+
+
+def test_Entity_init__rm_comp__fail_returns_False():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    result = e.rm_comp('z')
+    # Raise exception?
+
+
+def test_Entity_init__getattr__returns_component_value():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    e.add_comp(a=1)
+    assert e.a == 1
+
+
+def test_Entity_init__getattr__DNE_returns_None():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player')
+    with pytest.raises(AttributeError):
+        result = e.a
+
+
+def test_Entity_init__blocks_component():
+    e = Entity(x=0, y=0, char='@', color=None, name='Player', blocks=True)
+
+    assert e.blocks
+    assert 'blocks' in e.components
 
 
 def test_Entity_move():
