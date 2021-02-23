dd57c301a3e09b9f7c7fa92bc89dde1eade3b016
Erik Lunna <eslunna@gmail.com>
2021-02-22 19:44:45 -0600

    Organized procgen by alphabetical order.

--- src/procgen.py
+++ src/procgen.py
@@ -9,106 +9,31 @@ import random
 import tcod
 
 
-def place_items(room, dungeon, floor_number):
-    number_of_items = random.randint(
-        0, get_max_value_for_floor(settings.max_items_by_floor, floor_number)
-    )
+def connect_2_doors(new_map, door1, door2):
+    # To connect the doors, we have to connect the closets!
+    x1, y1 = door1.closet()
+    x2, y2 = door2.closet()
 
-    items = get_entities_at_random(
-        factory.item_chances, number_of_items, floor_number
-    )
+    # Choose a method of creating the tunnel:
+    path = get_L_path((x1, y1), (x2, y2))
 
-    for entity in items:
-        x, y = room.random_point_inside()
-        # We don't care if they stack on the map
-        entity.spawn(dungeon, x, y)
+    # Draw a diagonal
+    # path = diagonal_tunnel(start, end):
 
+    for x, y in path:
+        # Stop drawing if we run into room corners.
+        if new_map.tiles[x, y] in tiles.room_corners:
+            return False
 
-def place_monsters(room, dungeon, floor_number):
-    number_of_monsters = random.randint(
-        0, get_max_value_for_floor(settings.max_monsters_by_floor, floor_number)
-    )
+        # Do not draw over inner room floors
+        if new_map.tiles[x, y] == tiles.room_floor:
+            return False
 
-    monsters = get_entities_at_random(
-        factory.enemy_chances, number_of_monsters, floor_number
-    )
+    # Dig out a tunnel between this room and the previous one.
+    for x, y in path:
+        new_map.tiles[x, y] = tiles.floor
 
-    for entity in monsters:
-        x = random.randint(room.x1 + 1, room.x2 - 2)
-        y = random.randint(room.y1 + 1, room.y2 - 2)
-
-        # Don't spawn them on top of each other.
-        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
-            entity.spawn(dungeon, x, y)
-
-
-def generate_map(max_rooms, room_min_size, room_max_size, map_width, map_height, engine):
-    """Generate a new dungeon map with rooms, corridors, and stairs.."""
-    new_map = gamemap.GameMap(engine, map_width, map_height)
-
-    # Create all the rects for the rooms
-    generate_rooms(new_map, max_rooms, room_min_size, room_max_size)
-
-    # Create the room coordinates for easy reference.
-    new_map.room_coords = new_map.room_coordinates()
-
-    # Connect the rooms with corridors
-    connecting_algorithm(new_map)
-
-    # Place doors
-    draw_doors(new_map)
-
-    # Put the upstair in the first room generated
-    center_of_first_room = new_map.rooms[0].center
-    new_map.tiles[center_of_first_room] = tiles.up_stairs
-    new_map.upstairs_location = center_of_first_room
-
-    # Put the downstair in the last room generated
-    center_of_last_room = new_map.rooms[-1].center
-    new_map.tiles[center_of_last_room] = tiles.down_stairs
-    new_map.downstairs_location = center_of_last_room
-    return new_map
-
-
-def connecting_algorithm(new_map):
-    # First connect rooms with a minimum spanning tree.
-    edges = min_spanning_tree_for_rooms(new_map.rooms)
-    for room1, room2 in edges:
-        connect_room_to_room(new_map, room1, room2)
-
-    # We'll perform a second round of connections to make the map easier to traverse.
-    # Try to add 1/2 of the room count as extra connections.
-    extra_connections = len(new_map.rooms) // 2
-
-    for i in range(extra_connections):
-        room1 = random.choice(new_map.rooms)
-        room2 = get_nearest_unconnected_room(new_map, room1)
-        connect_room_to_room(new_map, room1, room2)
-
-
-def draw_doors(new_map):
-    """ Drawing doors needs to be a separate activity done last after corridors, because if it's combined with
-    corridor drawing, there are conflicts in where doors and floor appear.
-    """
-    for d in new_map.doors:
-        valid_door = True
-        for direction in settings.CARDINAL_DIR.values():
-            dx, dy = direction
-
-            # Check around for other doors.
-            if new_map.tiles[d.x + dx][d.y + dy] == tiles.door:
-                # Oh no, a door is adjacent! Abort mission!
-                valid_door = False
-                continue
-
-        if valid_door:
-            new_map.tiles[d.x, d.y] = tiles.door
-
-            # Is the closet wall yet?
-            closet_x, closet_y = d.closet()
-            if new_map.tiles[closet_x, closet_y] == tiles.wall:
-                # Dig out the closet
-                new_map.tiles[closet_x, closet_y] = tiles.floor
+    return True
 
 
 def connect_room_to_room(new_map, room1, room2):
@@ -161,6 +86,150 @@ def connect_room_to_room(new_map, room1, room2):
         room2.connections.append(room1.label)
 
 
+def connecting_algorithm(new_map):
+    # First connect rooms with a minimum spanning tree.
+    edges = min_spanning_tree_for_rooms(new_map.rooms)
+    for room1, room2 in edges:
+        connect_room_to_room(new_map, room1, room2)
+
+    # We'll perform a second round of connections to make the map easier to traverse.
+    # Try to add 1/2 of the room count as extra connections.
+    extra_connections = len(new_map.rooms) // 2
+
+    for i in range(extra_connections):
+        room1 = random.choice(new_map.rooms)
+        room2 = get_nearest_unconnected_room(new_map, room1)
+        connect_room_to_room(new_map, room1, room2)
+
+
+def dig_Astar_path(new_map, door1, door2):
+    # Get the closets outside the doors
+    x1, y1 = door1.closet()
+    x2, y2 = door2.closet()
+
+    # A* path
+    path = get_path_to(new_map, x1, y1, x2, y2)
+    # If there is only a single point - the path is not able to complete
+    if len(path) == 1:
+        return False
+
+    # Chck the path
+    for point in path:
+        x, y = point
+
+        # We won't allow drawing over room walls or doors.
+        if new_map.tiles[x, y] in tiles.room_walls:
+            return False
+
+    # Draw the path
+    for point in path:
+        new_map.tiles[point] = tiles.floor
+    return True
+
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def draw_doors(new_map):
+    """ Drawing doors needs to be a separate activity done last after corridors, because if it's combined with
+    corridor drawing, there are conflicts in where doors and floor appear.
+    """
+    for d in new_map.doors:
+        valid_door = True
+        for direction in settings.CARDINAL_DIR.values():
+            dx, dy = direction
+
+            # Check around for other doors.
+            if new_map.tiles[d.x + dx][d.y + dy] == tiles.door:
+                # Oh no, a door is adjacent! Abort mission!
+                valid_door = False
+                continue
+
+        if valid_door:
+            new_map.tiles[d.x, d.y] = tiles.door
+
+            # Is the closet wall yet?
+            closet_x, closet_y = d.closet()
+            if new_map.tiles[closet_x, closet_y] == tiles.wall:
+                # Dig out the closet
+                new_map.tiles[closet_x, closet_y] = tiles.floor
+
+
+def generate_map(max_rooms, room_min_size, room_max_size, map_width, map_height, engine):
+    """Generate a new dungeon map with rooms, corridors, and stairs.."""
+    new_map = gamemap.GameMap(engine, map_width, map_height)
+
+    # Create all the rects for the rooms
+    generate_rooms(new_map, max_rooms, room_min_size, room_max_size)
+
+    # Create the room coordinates for easy reference.
+    new_map.room_coords = new_map.room_coordinates()
+
+    # Connect the rooms with corridors
+    connecting_algorithm(new_map)
+
+    # Place doors
+    draw_doors(new_map)
+
+    # Put the upstair in the first room generated
+    center_of_first_room = new_map.rooms[0].center
+    new_map.tiles[center_of_first_room] = tiles.up_stairs
+    new_map.upstairs_location = center_of_first_room
+
+    # Put the downstair in the last room generated
+    center_of_last_room = new_map.rooms[-1].center
+    new_map.tiles[center_of_last_room] = tiles.down_stairs
+    new_map.downstairs_location = center_of_last_room
+    return new_map
+
+
+def generate_random_room(map_width, map_height, min_size, max_size):
+    room_width = random.randint(min_size, max_size)
+    room_height = random.randint(min_size, max_size)
+
+    x = random.randint(0, map_width - room_width - 1)
+    y = random.randint(0, map_height - room_height - 1)
+
+    return rect.Rect(x, y, room_width, room_height)
+
+
+def generate_rooms(new_map, max_rooms, room_min_size, room_max_size):
+    for r in range(max_rooms):
+        new_room = generate_random_room(
+            map_width=new_map.width,
+            map_height=new_map.height,
+            min_size=room_min_size,
+            max_size=room_max_size
+        )
+
+        # Run through the other rooms and see if they intersect with this one.
+        if any(new_room.intersects(other_room) for other_room in new_map.rooms):
+            continue  # This room intersects, so go to the next attempt.
+
+        # Dig out this rooms inner area.
+        new_map.tiles[new_room.inner] = tiles.room_floor
+
+        # Draw walls
+        for point in new_room.horz_walls():
+            new_map.tiles[point] = tiles.room_horz_wall
+        for point in new_room.vert_walls():
+            new_map.tiles[point] = tiles.room_vert_wall
+
+        # Draw corners (must be ordered after walls)
+        new_map.tiles[new_room.ne_corner] = tiles.room_ne_corner
+        new_map.tiles[new_room.nw_corner] = tiles.room_nw_corner
+        new_map.tiles[new_room.se_corner] = tiles.room_se_corner
+        new_map.tiles[new_room.sw_corner] = tiles.room_sw_corner
+
+        # Label the room to match it's index in new_map.rooms
+        label = len(new_map.rooms)
+        new_room.label = label
+
+        # Add this room to the map's list.
+        new_map.rooms.append(new_room)
+
+
 def get_valid_pair_of_doors(matches):
     """ Iterates through a list of facing doors and picks until we find a valid choice.
     """
@@ -202,58 +271,6 @@ def get_valid_pair_of_doors(matches):
         return pair
 
 
-def connect_2_doors(new_map, door1, door2):
-    # To connect the doors, we have to connect the closets!
-    x1, y1 = door1.closet()
-    x2, y2 = door2.closet()
-
-    # Choose a method of creating the tunnel:
-    path = get_L_path((x1, y1), (x2, y2))
-
-    # Draw a diagonal
-    # path = diagonal_tunnel(start, end):
-
-    for x, y in path:
-        # Stop drawing if we run into room corners.
-        if new_map.tiles[x, y] in tiles.room_corners:
-            return False
-
-        # Do not draw over inner room floors
-        if new_map.tiles[x, y] == tiles.room_floor:
-            return False
-
-    # Dig out a tunnel between this room and the previous one.
-    for x, y in path:
-        new_map.tiles[x, y] = tiles.floor
-
-    return True
-
-
-def dig_Astar_path(new_map, door1, door2):
-    # Get the closets outside the doors
-    x1, y1 = door1.closet()
-    x2, y2 = door2.closet()
-
-    # A* path
-    path = get_path_to(new_map, x1, y1, x2, y2)
-    # If there is only a single point - the path is not able to complete
-    if len(path) == 1:
-        return False
-
-    # Chck the path
-    for point in path:
-        x, y = point
-
-        # We won't allow drawing over room walls or doors.
-        if new_map.tiles[x, y] in tiles.room_walls:
-            return False
-
-    # Draw the path
-    for point in path:
-        new_map.tiles[point] = tiles.floor
-    return True
-
-
 def get_closest_pair_of_doors(matches):
     # Find the pair of doors that are the closest in distance.
     closest_pair = None
@@ -292,59 +309,6 @@ def get_nearest_unconnected_room(new_map, room):
 
             # Passed all checks!
             return result
-
-
-def populate_map(new_map, engine):
-    for room in new_map.rooms:
-        # Populate the room with monsters and items
-        place_monsters(room, new_map, engine.game_world.current_floor)
-        place_items(room, new_map, engine.game_world.current_floor)
-
-
-def generate_rooms(new_map, max_rooms, room_min_size, room_max_size):
-    for r in range(max_rooms):
-        new_room = generate_random_room(
-            map_width=new_map.width,
-            map_height=new_map.height,
-            min_size=room_min_size,
-            max_size=room_max_size
-        )
-
-        # Run through the other rooms and see if they intersect with this one.
-        if any(new_room.intersects(other_room) for other_room in new_map.rooms):
-            continue  # This room intersects, so go to the next attempt.
-
-        # Dig out this rooms inner area.
-        new_map.tiles[new_room.inner] = tiles.room_floor
-
-        # Draw walls
-        for point in new_room.horz_walls():
-            new_map.tiles[point] = tiles.room_horz_wall
-        for point in new_room.vert_walls():
-            new_map.tiles[point] = tiles.room_vert_wall
-
-        # Draw corners (must be ordered after walls)
-        new_map.tiles[new_room.ne_corner] = tiles.room_ne_corner
-        new_map.tiles[new_room.nw_corner] = tiles.room_nw_corner
-        new_map.tiles[new_room.se_corner] = tiles.room_se_corner
-        new_map.tiles[new_room.sw_corner] = tiles.room_sw_corner
-
-        # Label the room to match it's index in new_map.rooms
-        label = len(new_map.rooms)
-        new_room.label = label
-
-        # Add this room to the map's list.
-        new_map.rooms.append(new_room)
-
-
-def generate_random_room(map_width, map_height, min_size, max_size):
-    room_width = random.randint(min_size, max_size)
-    room_height = random.randint(min_size, max_size)
-
-    x = random.randint(0, map_width - room_width - 1)
-    y = random.randint(0, map_height - room_height - 1)
-
-    return rect.Rect(x, y, room_width, room_height)
 
 
 # noinspection PyTypeChecker
@@ -497,5 +461,42 @@ def min_spanning_tree_for_rooms(rooms):
     return edges
 
 
-def distance(x1, y1, x2, y2):
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+def place_items(room, dungeon, floor_number):
+    number_of_items = random.randint(
+        0, get_max_value_for_floor(settings.max_items_by_floor, floor_number)
+    )
+
+    items = get_entities_at_random(
+        factory.item_chances, number_of_items, floor_number
+    )
+
+    for entity in items:
+        x, y = room.random_point_inside()
+        # We don't care if they stack on the map
+        entity.spawn(dungeon, x, y)
+
+
+def place_monsters(room, dungeon, floor_number):
+    number_of_monsters = random.randint(
+        0, get_max_value_for_floor(settings.max_monsters_by_floor, floor_number)
+    )
+
+    monsters = get_entities_at_random(
+        factory.enemy_chances, number_of_monsters, floor_number
+    )
+
+    for entity in monsters:
+        x = random.randint(room.x1 + 1, room.x2 - 2)
+        y = random.randint(room.y1 + 1, room.y2 - 2)
+
+        # Don't spawn them on top of each other.
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            entity.spawn(dungeon, x, y)
+
+
+def populate_map(new_map, engine):
+    for room in new_map.rooms:
+        # Populate the room with monsters and items
+        place_monsters(room, new_map, engine.game_world.current_floor)
+        place_items(room, new_map, engine.game_world.current_floor)
