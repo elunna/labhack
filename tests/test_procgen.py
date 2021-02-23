79122484af0a82f76756621708502aa4b3ba91aa
Erik Lunna <eslunna@gmail.com>
2021-02-22 19:01:17 -0600

    Added tests for generate_random_room

--- tests/test_procgen.py
+++ tests/test_procgen.py
@@ -2,8 +2,30 @@
 from src import procgen
 import pytest
 
-# place entities
-# generate_dungeon
+
+def test_generate_random_room__in_bounds():
+    map_width, map_height= 100, 50
+    min_size, max_size = 5, 10
+    r = procgen.generate_random_room(map_width, map_height, min_size, max_size)
+    assert r.x1 >= 0 and r.y1 >= 0
+    assert r.x2 < map_width
+    assert r.y2 < map_height
+
+
+def test_generate_random_room__within_max_size():
+    map_width, map_height= 100, 50
+    min_size, max_size = 5, 10
+    r = procgen.generate_random_room(map_width, map_height, min_size, max_size)
+    assert r.width <= max_size
+    assert r.height <= max_size
+
+
+def test_generate_random_room__within_min_size():
+    map_width, map_height = 100, 50
+    min_size, max_size = 5, 10
+    r = procgen.generate_random_room(map_width, map_height, min_size, max_size)
+    assert r.width >= min_size
+    assert r.height >= min_size
 
 
 def test_get_L_path__horz_first():
