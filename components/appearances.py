f49166a0bace763f16c69adf34c059aa3a1606b3
Erik Lunna <eslunna@gmail.com>
2021-02-25 22:56:01 -0600

    Added a file of appearances we can use for item descriptions.

---
+++ components/appearances.py
@@ -0,0 +1,18 @@
+potion_appearances = [
+    'sky blue',
+    'crimson',
+    'orange',
+    'greenish',
+    'glowing',
+    'milky',
+    'shady',
+    'dark brown',
+    'watery'
+]
+
+scroll_appearances = [
+    'yabba dabba doo',
+    'xid xad xoo',
+    'tythic nupitus',
+    'yupiter qomens'
+]
