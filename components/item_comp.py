cd1d8828d55fc9f045a123d1dd15d5babd37f517
Erik Lunna <eslunna@gmail.com>
2021-03-01 02:02:24 -0600

    Fleshed out stacking functionality in the ItemComponent.

--- components/item_comp.py
+++ components/item_comp.py
@@ -1,4 +1,7 @@
+import copy
+
 from .component import Component
+from src import exceptions
 
 
 class ItemComponent(Component):
@@ -11,3 +14,39 @@ class ItemComponent(Component):
 
         # TODO: Can we limit access to this if the item isn't stackable?
         self.stacksize = 1
+
+    def merge_stack(self, other):
+        """ Merge another stackable item of the same type into this one.
+            This stack gains an amount equal to the other stack's size
+            The other stack & item is destroyed.
+            returns True if the operation succeeded, False if it did not.
+        """
+        if not self.stackable:
+            raise exceptions.Impossible("Item is not stackable!")
+
+    def split_stack(self, qty):
+        """ Splits this items stack into a new Item with a stack of qty size.
+            Cannot split more than the stack size.
+            Returns a new Item if successful, None if not.
+        """
+        if not self.stackable:
+            raise exceptions.Impossible("Item is not stackable!")
+        elif qty < 1 or qty >= self.stacksize:
+            raise ValueError("split_stack requires positive integer for qty!")
+        self.deplete_stack(qty)
+        new_item = copy.deepcopy(self.parent)
+        new_item.item.stacksize = qty
+        return new_item
+
+    def deplete_stack(self, qty):
+        """ Consumes a portion (or all) of the items in this stack
+            If all the items are used up, the Item is destroyed.
+            Returns True if successful, False otherwise.
+        """
+        if not self.stackable:
+            raise exceptions.Impossible("Item is not stackable!")
+        elif qty < 1 or qty > self.stacksize:
+            raise ValueError("deplete stack must be within the stacksize!")
+
+        self.stacksize -= qty
+        return True
--- components/test_item_comp.py
+++ components/test_item_comp.py
@@ -1,19 +1,29 @@
 import pytest
+
+from src.entity import Entity
 from .item_comp import ItemComponent
 from .component import Component
+from src import exceptions
 
 
 @pytest.fixture
-def test_item():
+def itemcomp():
     return ItemComponent()
 
 
-def test_init__is_Component(test_item):
-    assert isinstance(test_item, Component)
+@pytest.fixture
+def testitem():
+    e = Entity(name="fleepgork", item=ItemComponent(stackable=True))
+    e.item.stacksize = 10
+    return e
 
 
-def test_init__stackable__False_by_default(test_item):
-    assert test_item.stackable is False
+def test_init__is_Component(itemcomp):
+    assert isinstance(itemcomp, Component)
+
+
+def test_init__stackable__False_by_default(itemcomp):
+    assert itemcomp.stackable is False
 
 
 def test_init__stackable_arg():
@@ -21,8 +31,8 @@ def test_init__stackable_arg():
     assert i.stackable
 
 
-def test_init__breakable__0_by_default(test_item):
-    assert test_item.breakable == 0
+def test_init__breakable__0_by_default(itemcomp):
+    assert itemcomp.breakable == 0
 
 
 def test_init__breakable_arg():
@@ -49,7 +59,96 @@ def test_init__set_stacksize_when_not_stackable():
         i.stacksize = 3
 
 
-def test_init__last_letter__None_by_default(test_item):
-    assert test_item.last_letter is None
+def test_init__last_letter__None_by_default(itemcomp):
+    assert itemcomp.last_letter is None
 
 
+def test_merge_stack__not_stackable__raises_Impossible():
+    i = ItemComponent(stackable=False)
+    with pytest.raises(exceptions.Impossible):
+        i.merge_stack('fake stack')
+
+# def test_merge_stack__identical_Item__returns_True():
+# def test_merge_stack__identical_Item__adds_to_stack():
+# def test_merge_stack__identical_Item__destroy_other_stack():
+# def test_merge_stack__different_Item__returns_False():
+
+
+def test_split_stack__not_stackable__raises_Impossible():
+    i = ItemComponent(stackable=False)
+    with pytest.raises(exceptions.Impossible):
+        i.split_stack(1)
+
+
+def test_split_stack__0_qty__raises_ValueError():
+    i = ItemComponent(stackable=True)
+    with pytest.raises(ValueError):
+        i.split_stack(0)
+
+
+def test_split_stack__partial__returns_copied_Item(testitem):
+    assert testitem.item.stacksize == 10
+    result = testitem.item.split_stack(1)
+    # Is the item the same type? Name?
+    assert result.name == testitem.name
+
+
+def test_split_stack__partial__stacksize_depleted(testitem):
+    assert testitem.item.stacksize == 10
+    testitem.item.split_stack(1)
+    assert testitem.item.stacksize == 9
+
+
+def test_split_stack__partial__copy_stacksize(testitem):
+    assert testitem.item.stacksize == 10
+    result = testitem.item.split_stack(1)
+    assert result.item.stacksize == 1
+
+
+def test_split_stack__full__raises_ValueError():
+    i = ItemComponent(stackable=True)
+    i.stacksize = 1
+    with pytest.raises(ValueError):
+        i.split_stack(1)
+
+
+def test_split_stack__more_than_stacksize__raises_ValueError():
+    i = ItemComponent(stackable=True)
+    assert i.stacksize == 1
+    with pytest.raises(ValueError):
+        i.split_stack(2)
+
+
+def test_deplete_stack__not_stackable__raises_Impossible():
+    i = ItemComponent(stackable=False)
+    with pytest.raises(exceptions.Impossible):
+        i.deplete_stack(1)
+
+
+def test_deplete_stack__0_qty__raises_ValueError():
+    i = ItemComponent(stackable=True)
+    with pytest.raises(ValueError):
+        i.deplete_stack(0)
+
+
+def test_deplete_stack__partial():
+    i = ItemComponent(stackable=True)
+    i.stacksize = 2
+    i.deplete_stack(1)
+    assert i.stacksize == 1
+
+
+def test_deplete_stack__success_returns_True():
+    i = ItemComponent(stackable=True)
+    i.stacksize = 2
+    assert i.deplete_stack(1)
+
+
+def test_deplete_stack__more_than_stacksize__raises_ValueError():
+    i = ItemComponent(stackable=True)
+    assert i.stacksize == 1
+    with pytest.raises(ValueError):
+        i.deplete_stack(2)
+
+
+# def test_deplete_stack__full__destroys_item():
