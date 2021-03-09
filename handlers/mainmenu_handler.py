a9468ea0ec412f0841e7847ad5aedb892a181a77
Erik Lunna <eslunna@gmail.com>
2021-03-08 20:54:28 -0600

    Separated PopupMsgHandler to its own file.

--- handlers/mainmenu_handler.py
+++ handlers/mainmenu_handler.py
@@ -1,6 +1,7 @@
 from src import rendering, settings
 from handlers.base_handler import BaseEventHandler
-from handlers.handlers import MainGameHandler, PopupMsgHandler
+from handlers.handlers import MainGameHandler
+from handlers.popup import PopupMsgHandler
 from handlers.mapdebugger import MapDebugHandler
 from src.setup_game import load_game, new_game
 import tcod
