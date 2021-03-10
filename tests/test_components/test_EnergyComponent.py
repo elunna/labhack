b85a1c3d253f3bc3b9f565bc4a2805b90b5604cf
Erik Lunna <eslunna@gmail.com>
2021-03-10 10:30:21 -0600

    Fixed bug in EnergyComponent tests where energy refill wasn't enough for an action.

--- tests/test_components/test_EnergyComponent.py
+++ tests/test_components/test_EnergyComponent.py
@@ -7,17 +7,17 @@ import pytest
 
 
 def test_init__is_Component():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     assert isinstance(ec, Component)
 
 
 def test_init__refill_arg():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     assert ec.threshold == ENERGY_THRESHOLD
 
 
 def test_init__energy_is_random():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     assert ec.energy >= 0
     assert ec.energy <= ENERGY_THRESHOLD
 
@@ -28,26 +28,26 @@ def test_init__0_refill__raises_ValueError():
 
 
 def test_add_energy():
-    ec = EnergyComponent(refill=10)
-    expected = ec.energy + 10
+    ec = EnergyComponent(refill=12)
+    expected = ec.energy + 12
     ec.add_energy()
     assert ec.energy == expected
 
 
 def test_burn_turn__unsuccessful_returns_False():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     ec.energy = 0  # Make sure this is 0 to force fail
     assert ec.burn_turn() is False
 
 
 def test_burn_turn__success_returns_True():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     ec.add_energy()
     assert ec.burn_turn() is True
 
 
 def test_burned_out__success_uses_energy():
-    ec = EnergyComponent(refill=10)
+    ec = EnergyComponent(refill=12)
     ec.energy = 12  # Force set this for testing. Minimum is 12 for a turn.
     ec.burn_turn()
     assert ec.energy == 0
