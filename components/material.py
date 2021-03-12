8448437f4aeec02393564092923ee8d914872a09
Erik Lunna <eslunna@gmail.com>
2021-03-12 10:16:45 -0600

    Added MaterialComponent with support for some erosion types and 3 levels of erosion.

--- components/material.py
+++ components/material.py
@@ -1,7 +1,111 @@
 from components.component import Component
 
 
+MATERIALS = [
+    "CLOTH", "COPPER", "GLASS", "IRON", "LEATHER", "PAPER", "PLASTIC", "WOOD",
+    # BONE?
+    # types of plastic
+    # RUBBER
+    # aluminium, acrylic, and dense plastics, poly-carbonate, epoxy,
+    # ABS, styrofoam
+]
+
+CORROSIONS = [
+    "BURN", "ROT", "MELT", "RUST", "CORRODE",
+    # RADIATED, IRRADIATED?, DULL
+]
+
+
 class MaterialComponent(Component):
-    """Defines the type of material that an item or entity is constructed from."""
-    def __init__(self, material):
+    """Defines the type of material that an item or entity is constructed from and manages it's level of
+    corrosion. The more corroded something is, the less effective it is and the more likely it is to break
+    or outright disintegrate.
+
+    Erosion_levels:
+        # 0 is no corrosion
+        # 1 is the first level of corrosion (no lingual modifier) ex: just "rusted"
+        # 2 is the second level: "very rusted"
+        # 3 is the third and final level of corrosion: "thoroughly rusted"
+
+        Examples:
+            A burned cloak has the same effectiveness as a -1 enchantment.
+            A very burned cloak has the same effectiveness as a -2 enchantment.
+            A thoroughly burned cloak has the same effectiveness as a -3 enchantment.
+            If an item with -3 erosion in a single type (like burned) is burned again - it is destroyed.
+
+    Vulnerabilities:
+        Some items are vulnerable to certain effects which instantly destroy the item
+            (and may cause other effects)
+        Glass is subject to sharp strikes and forces and may shatter.
+        Gas is subject to fire and may explode.
+
+    """
+    def __init__(self, material, *vulnerabilities):
+        if material not in MATERIALS:
+            raise ValueError(f"{material} is not a valid material!")
         self.material = material
+
+        if any(v for v in vulnerabilities if v not in CORROSIONS):
+            raise ValueError(f'MaterialComponent received invalid vulnerabilitiy!')
+
+        # Create a dict of the different valid erosions, start each one at 0.
+        self.erosion = {v: 0 for v in vulnerabilities}
+
+        # "SHATTER", "FREEZE", "ZAP/VAPORIZE - instantly destroys the item
+
+    def add_erosion(self, erosion_type):
+        if erosion_type in self.erosion:
+            if self.erosion[erosion_type] < 3:
+                self.erosion[erosion_type] += 1
+                return True
+        return False
+
+        # is it plastic and thoroughly melted?
+
+    # def fix(self, erosion):
+        # repairs one level of erosion
+        # return True
+
+    def burn(self):
+        return self.add_erosion("BURN")
+
+    def rot(self):
+        return self.add_erosion("ROT")
+
+    def melt(self):
+        return self.add_erosion("MELT")
+
+    def rust(self):
+        return self.add_erosion("RUST")
+
+    def corrode(self):
+        return self.add_erosion("CORRODE")
+
+    # def impact(self):  # forced/impact
+        # impact damage is a sharp physical force, which breaks delicate objects.
+        # return True
+
+    # def freeze(self):
+    #     return self.material == "GLASS"
+
+    # def is_zappable?
+    # 	is it a wand or ring? it will instantly vanish?
+
+    # def is_breakable(self):
+    #     return self.breakable
+
+        # (through normal usage or attacking)
+        # slightly broken (increased chance of fully breaking by 50%)
+        # Reduces effectiveness by 50%
+        # The breakable number means it has a 1 in x chance of breaking, 0 never breaks
+
+
+
+# subclasses of MaterialComponent
+# LeatherItem
+# 	vulnerabilities
+# WoodItem
+# IronItem
+# ClothItem
+# PaperItem
+
--- tests/test_components/test_MaterialComponent.py
+++ tests/test_components/test_MaterialComponent.py
@@ -4,13 +4,139 @@ from components.material import MaterialComponent
 
 
 def test_init__is_Component():
-    m = MaterialComponent('plastic')
+    m = MaterialComponent('PLASTIC', 'MELT')
     assert isinstance(m, Component)
 
 
 def test_init__material():
-    m = MaterialComponent('plastic')
-    assert m.material == 'plastic'
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.material == 'PLASTIC'
 
 
-# invalid material
+def test_init__vulnerabilities__single_tuple():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.erosion == {'MELT': 0}
+
+
+def test_init__vulnerabilities__tuple():
+    m = MaterialComponent('PLASTIC', 'MELT', 'BURN')
+    assert m.erosion == {'MELT': 0, 'BURN': 0}
+
+
+def test_init__invalid_material():
+    with pytest.raises(ValueError):
+        MaterialComponent('RUG', 'melt')
+
+
+def test_init__invalid_vulnerabilities():
+    with pytest.raises(ValueError):
+        MaterialComponent('CLOTH', 'LAUGH')
+
+
+def test_add_erosion__success_returns_True():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.add_erosion("MELT")
+
+
+def test_add_erosion__fail_returns_False():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.add_erosion("ROT") is False
+
+
+def test_add_erosion__0_to_1():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.erosion['MELT'] == 0
+    m.add_erosion("MELT")
+    assert m.erosion['MELT'] == 1
+
+
+def test_add_erosion__level_3__returns_False():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    m.erosion['MELT'] = 3
+    assert m.add_erosion("MELT") is False
+    assert m.erosion['MELT'] == 3
+
+
+def test_burn__burnable_returns_True():
+    m = MaterialComponent('WOOD', 'BURN')
+    assert m.burn()
+
+
+def test_burn__burnable_calls_add_erosion(mocker):
+    mocker.patch('components.material.MaterialComponent.add_erosion')
+    m = MaterialComponent('WOOD', 'BURN')
+    m.burn()
+    m.add_erosion.assert_called_once()
+
+
+def test_burn__nonburnable_returns_False():
+    m = MaterialComponent('IRON', 'RUST')
+    assert m.burn() is False
+
+
+def test_rot__rottable_returns_True():
+    m = MaterialComponent('WOOD', 'ROT')
+    assert m.rot()
+
+
+def test_rot__rottable_calls_add_erosion(mocker):
+    mocker.patch('components.material.MaterialComponent.add_erosion')
+    m = MaterialComponent('WOOD', 'ROT')
+    m.rot()
+    m.add_erosion.assert_called_once()
+
+
+def test_rot__nonrottable_returns_False():
+    m = MaterialComponent('IRON', 'RUST')
+    assert m.rot() is False
+
+
+def test_melt__meltable_returns_True():
+    m = MaterialComponent('PLASTIC', 'MELT')
+    assert m.melt()
+
+
+def test_melt__meltable_calls_add_erosion(mocker):
+    mocker.patch('components.material.MaterialComponent.add_erosion')
+    m = MaterialComponent('PLASTIC', 'MELT')
+    m.melt()
+    m.add_erosion.assert_called_once()
+
+
+def test_melt__nonmeltable_returns_False():
+    m = MaterialComponent('WOOD', 'BURN')
+    assert m.melt() is False
+
+
+def test_rust__rustable_returns_True():
+    m = MaterialComponent('IRON', 'RUST')
+    assert m.rust()
+
+
+def test_rust__rustable_calls_add_erosion(mocker):
+    mocker.patch('components.material.MaterialComponent.add_erosion')
+    m = MaterialComponent('IRON', 'RUST')
+    m.rust()
+    m.add_erosion.assert_called_once()
+
+
+def test_rust__nonrustable_returns_False():
+    m = MaterialComponent('WOOD', 'BURN')
+    assert m.rust() is False
+
+
+def test_corrode__corrodable_returns_True():
+    m = MaterialComponent('IRON', 'RUST', 'CORRODE')
+    assert m.corrode()
+
+
+def test_corrode__corrodable_calls_add_erosion(mocker):
+    mocker.patch('components.material.MaterialComponent.add_erosion')
+    m = MaterialComponent('IRON', 'RUST')  # No corrode, means always calls add_erosion
+    m.corrode()
+    m.add_erosion.assert_called_once()
+
+
+def test_corrode__noncorrodable_returns_False():
+    m = MaterialComponent('WOOD', 'BURN')
+    assert m.corrode() is False
